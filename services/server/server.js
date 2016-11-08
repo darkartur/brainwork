e0a5d561ce2fc9040bed3404b7bcc30e16919c00
Arthur Udalov <AEUdalov.SBT@sberbank.ru>
2016-11-09 00:11:07 +0300

    Еще немного плохих файлов

--- services/server/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-(function (HTTPMethod) {
-    HTTPMethod[HTTPMethod["GET"] = 0] = "GET";
-    HTTPMethod[HTTPMethod["POST"] = 1] = "POST";
-})(exports.HTTPMethod || (exports.HTTPMethod = {}));
-var HTTPMethod = exports.HTTPMethod;
-//# sourceMappingURL=server.js.map
